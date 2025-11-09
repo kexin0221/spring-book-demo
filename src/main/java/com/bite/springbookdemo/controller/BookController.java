898602a4e589040a7f08ab4f2269b9614e6a8ec1
mou-kexin <2584857586@qq.com>
2025-11-09 21:09:43 +0800

    spring-book-demo

--- src/main/java/com/bite/springbookdemo/controller/BookController.java
+++ src/main/java/com/bite/springbookdemo/controller/BookController.java
@@ -2,12 +2,13 @@ package com.bite.springbookdemo.controller;
 
 import com.bite.springbookdemo.dao.BookInfo;
 import com.bite.springbookdemo.service.BookService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
 
+@Slf4j
 @RequestMapping("/book")
 @RestController
 public class BookController {
@@ -22,23 +23,30 @@ public class BookController {
 
     @RequestMapping("/addBook")
     public String addBook(BookInfo bookInfo) {
+        log.info("添加图书, request: {}", bookInfo);
         //1. 参数校验
         if (!StringUtils.hasLength(bookInfo.getBookName())) {
+            log.error("添加图书，参数不合法，request: {}", bookInfo);
             return "书名不能为空！";
         }
         if (!StringUtils.hasLength(bookInfo.getAuthor())) {
+            log.error("添加图书，参数不合法，request: {}", bookInfo);
             return "作者不能为空！";
         }
         if (bookInfo.getCount() == null) {
+            log.error("添加图书，参数不合法，request: {}", bookInfo);
             return "数量不能为空！";
         }
         if (bookInfo.getPrice() == null) {
+            log.error("添加图书，参数不合法，request: {}", bookInfo);
             return "价格不能为空！";
         }
         if (!StringUtils.hasLength(bookInfo.getPublish())) {
+            log.error("添加图书，参数不合法，request: {}", bookInfo);
             return "出版社不能为空！";
         }
         if (bookInfo.getStatus() == null) {
+            log.error("添加图书，参数不合法，request: {}", bookInfo);
             return "状态码不能为空！";
         }
         //2. 存储数据
@@ -47,6 +55,7 @@ public class BookController {
             bookService.addBook(bookInfo);
             return "";
         } catch (Exception e) {
+            log.error("添加图书异常, e: {}", String.valueOf(e));
             return "添加图书发生异常！";
         }
 
--- src/main/java/com/bite/springbookdemo/mapper/BookInfoMapper.java
+++ src/main/java/com/bite/springbookdemo/mapper/BookInfoMapper.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface BookInfoMapper {
 
-    @Insert("insert into book_info (book_name, author, count, price, publish) " +
-            "values (#{bookName}, #{author}, #{count}, #{price}, #{publish})")
+    @Insert("insert into book_info (book_name, author, count, price, publish, status) " +
+            "values (#{bookName}, #{author}, #{count}, #{price}, #{publish}, #{status})")
     Integer addBook(BookInfo bookInfo);
 }
